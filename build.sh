4091ccda68b2cf9a97334a01aa1595ef88b4c593
Jeroen <jeroenooms@gmail.com>
2018-06-28 11:32:02 +0200

    trigger build

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+cd "$(dirname "$0")"
+
+# Update system
 pacman -Syyu
 pacman -S --needed --noconfirm mingw-w64-{i686,x86_64}-{gcc,gcc-fortran}
 pacman -S --needed --noconfirm mingw-w64-{i686,x86_64}-{icu,libtiff,libjpeg,libpng,pcre,xz,bzip2,zlib}
