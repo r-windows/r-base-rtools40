6df5aedc8c1e25c8ce63b927fbe492d83105ff43
Jeroen <jeroenooms@gmail.com>
2018-06-29 14:24:49 +0200

    No need to build for both architectures

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ pacman -S --needed --noconfirm mingw-w64-{i686,x86_64}-{gcc,gcc-fortran}
 pacman -S --needed --noconfirm mingw-w64-{i686,x86_64}-{icu,libtiff,libjpeg,libpng,pcre,xz,bzip2,zlib}
 pacman -S --needed --noconfirm mingw-w64-{i686,x86_64}-{cairo,tk,curl}
 
-# Build package
-makepkg-mingw
+# Build package (only once)
+MINGW_INSTALLS="mingw64" makepkg-mingw
