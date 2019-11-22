63350d80897b004ee10eb7e3864ecedb54c1a68a
Jeroen Ooms <jeroenooms@gmail.com>
2019-11-22 20:02:18 +0100

    try with printsrcinfo

--- full-build.sh
+++ full-build.sh
@@ -7,4 +7,5 @@ rm -rf src pkg
 # Build package (only once)
 set -o pipefail
 pacman -Syyu --noconfirm
-MINGW_INSTALLS="mingw64" makepkg-mingw --noconfirm --noprogressbar --skippgpcheck --nocheck --syncdeps --rmdeps --cleanbuild 2>&1 | tee r-devel.log
+MINGW_INSTALLS="mingw32" makepkg-mingw --noconfirm --noprogressbar --skippgpcheck --nocheck --syncdeps --printsrcinfo
+MINGW_INSTALLS="mingw64" makepkg-mingw --noconfirm --noprogressbar --skippgpcheck --nocheck --syncdeps --cleanbuild 2>&1 | tee r-devel.log
