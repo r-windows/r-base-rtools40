a16d3a4973d65c3cbb84c8a3c6cc299dc99687ed
Jeroen Ooms <jeroenooms@gmail.com>
2019-11-22 14:18:18 +0100

    automatially install required dependencies with syncdeps

--- full-build.sh
+++ full-build.sh
@@ -4,12 +4,7 @@ cd "$(dirname "$0")"
 # Cleanup
 rm -rf src pkg
 
-# Update system
-pacman -Syyu --noconfirm
-pacman -S --needed --noconfirm mingw-w64-{i686,x86_64}-{gcc,gcc-fortran}
-pacman -S --needed --noconfirm mingw-w64-{i686,x86_64}-{icu,libtiff,libjpeg,libpng,pcre,xz,bzip2,zlib}
-pacman -S --needed --noconfirm mingw-w64-{i686,x86_64}-{cairo,tk,curl}
-
 # Build package (only once)
 set -o pipefail
-MINGW_INSTALLS="mingw64" makepkg-mingw 2>&1 | tee r-devel.log
+pacman -Syyu --noconfirm
+MINGW_INSTALLS="mingw64" makepkg-mingw --noconfirm --noprogressbar --skippgpcheck --nocheck --syncdeps --rmdeps --cleanbuild 2>&1 | tee r-devel.log
